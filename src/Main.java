a997ab4dcbbba00be21d74eb9f03b8fcdf60030a
AustinMitchell <n.austin.mitchell@gmail.com>
2017-03-04 21:35:09 -0500

    Untracking bin/; Using worker threads to make map generation non-blocking

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,90 @@ import simple.run.*;
 import java.awt.Color;
 
 public class Main extends SimpleGUIApp {
+	interface Worker<ResultType> {
+		ResultType doWork();
+	}
+	class WorkerThread<ResultType> implements Runnable {
+		private Worker<ResultType> worker;
+		private boolean finished;
+		private ResultType result;
+		
+		public WorkerThread(Worker<ResultType> worker) {
+			this.worker = worker;
+			finished   = false;
+		}
+		
+		public void run() {
+			finished = false;
+			result = worker.doWork();
+			finished = true;
+		}
+		
+		public boolean isFinished() { return finished; }
+		public ResultType getResult() { finished = false; return result; }
+	}
+	
 	public static void main(String[]args) { Main.start(new Main(), "Map Generator"); }
 	public Main() { super(1025, 1025, 30); }
 	
-	static final int depth = 5;
+	static final int depth = 13;
 	static final int size = (int)Math.pow(2, depth) + 1;
 	static final float min = 0;
 	static final float max = 255;
 	static final float h = 0.5f;
 	
 	Map map;
+	WorkerThread<Map> generatorThread;
+	WorkerThread<Image> converterThread;
 	Button reset;
 	Image image;
 	
+	boolean loading;
+	
 	public void setup() {
+		reset = new Button(800, 100, 100, 50, "Generating...", null);
+		reset.setFillColor(new Color(255, 200, 200));
+		
 		image = new Image(size, size);
-		map = new Map(depth, min, max, h); 
-		reset = new Button(800, 100, 100, 50, "Reset", null);
-		convertToImage();
+		
+		generatorThread = new WorkerThread<Map>(new Worker<Map>() {
+				public Map doWork() {
+					return new Map(depth, min, max, h);
+				}
+			});
+		converterThread = new WorkerThread<Image>(new Worker<Image>() {
+				public Image doWork() {
+					Image image = new Image(size, size);
+					convertToImage(image);
+					return image;
+				}
+			});
+		
+		loading = true;
+		new Thread(generatorThread).start();
+		
 	}
 	
 	public void loop() {
 		reset.update();
-		if (reset.isClicked()) {
-			map.reset(depth, min, max, h); 
-			convertToImage();
-		} else {
-			/*
-			Map.shiftMap(depth, min, max, h, map.heightMap);
-			convertToImage(); */
+		if (reset.isClicked() && !loading) {
+			loading = true;
+			reset.setText("Generating...");
+			reset.setFillColor(new Color(255, 200, 200));
+			new Thread(generatorThread).start();
+		}
+		
+		if (generatorThread.isFinished()) {
+			reset.setText("Converting...");
+			map = generatorThread.getResult();
+			reset.setFillColor(new Color(255, 255, 200));
+			new Thread(converterThread).start();
+		}
+		if (converterThread.isFinished()) {
+			loading = false;
+			reset.setText("Reset");
+			reset.setFillColor(new Color(255, 255, 255));
+			image = converterThread.getResult();
 		}
 		
 		draw.image(image, 0, 0);
@@ -39,11 +94,11 @@ public class Main extends SimpleGUIApp {
 		updateView();
 	}
 	
-	void convertToImage() {
+	void convertToImage(Image image) {
 		for (int i=0; i<size; i++) {
 			for (int j=0; j<size; j++) {
 				int val = (int)map.get(i, j);
-				image.set(i, j, toFireColor(val));
+				image.setColor(i, j, toFireColor(val));
 			}
 		}
 	}
